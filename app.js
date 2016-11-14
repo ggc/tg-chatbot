8d4843f42c155a21d07dfa82fe6b06a90783ef27
GabrielGC <gabriel.galan.17@gmail.com>
2016-11-14 14:29:52 +0100

    ADD first stage Bot. Answers with "Welcome <yourname>"

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express'),
+	path = require('path'),
+	favicon = require('serve-favicon'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	TeleBot = require('telebot');
+
+
+var index = require('./routes/index'),
+	users = require('./routes/users');
+
+var app = express();
+//TODO: Take out token
+var bot = new TeleBot({
+	token: '286209648:AAGGOAddklYJphod4lg-u6gyfhKZzr3mgMw'
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+bot.on('text', msg => {
+	let fromId = msg.from.id;
+	let firstName = msg.from.first_name;
+	let reply = msg.message_id;
+	return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+});
+
+bot.connect();
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
