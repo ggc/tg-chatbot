9f894179b7cc9b393a87f9a5692404a471dd6761
GabrielGC <gabriel.galan.17@gmail.com>
2016-11-15 14:01:52 +0100

    Add periods and empty command skeleton

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ var bot = new TeleBot({
 	token: process.env.TG_TOKEN
 });
 
+var workTime;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -33,11 +35,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+var timerId;
+function work(msg) {
+	bot.sendMessage(msg.chat.id, '> Prepare to sprint');
+	timerId = setTimeout(()=>{
+		rest(msg);
+	}, 30*1000);
+};
+function rest(msg) {
+	bot.sendMessage(msg.chat.id, '> Rest');
+	timerId = setTimeout(()=>{
+		work(msg);
+	}, 5*1000);
+};
+
+// Use this to log every message
 bot.on('text', msg => {
-	var fromId = msg.from.id;
-	var firstName = msg.from.first_name;
-	var reply = msg.message_id;
-	return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+
+	console.log(`[msg] ${ msg.chat.id } ${ msg.text }`);
+});
+// Shows a brief description about bot
+bot.on('/about', msg => {
+	return bot.sendMessage(msg.chat.id, 'This is suppose to improve work');
+});
+// Start the pomodoro timer. Starts on Rest mode, of course
+//TODO: Move to /start command
+bot.on('/work', msg => {
+	work(msg);
+})
+// Ends pomodoro timer
+bot.on('/rest', msg => {
+	clearTimeout(timerId);
+	return bot.sendMessage(msg.chat.id, `You can go home, ${msg.from.first_name}`);	
+});
+// Command to set time intervals and modes
+bot.on('/config', msg => {
+
+});
+// Add task to the DB
+bot.on('/add', msg => {
+	
+});
+// Show own tasks
+bot.on('/show', msg => {
+	
 });
 
 bot.connect();
