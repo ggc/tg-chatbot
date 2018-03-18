e64843f5049b5af5352b1d6ab7a10e2bee776ba9
ggc <gabriel.galan.17@gmail.com>
2018-03-18 15:18:07 +0100

    [MV] Take out scraper to a folder

--- go-bot/main.go
+++ go-bot/main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
 
+	"./scraper"
 	"./server"
-	"github.com/gocolly/colly"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
 func main() {
-	chatText := make(chan string, 10)
+	msgChannel := make(chan string, 10)
 	serverRunning := make(chan bool)
 
 	/* Start a server to provide content */
@@ -19,36 +18,7 @@ func main() {
 	<-serverRunning
 
 	/* Fetch the content */
-	fmt.Println("Starting scraping process...")
-	c := colly.NewCollector()
-
-	// c.OnHTML(".row-fluid", func(e *colly.HTMLElement) {
-	c.OnHTML("div[id=result-set]", func(e *colly.HTMLElement) {
-		fmt.Println("found a list of items")
-		e.ForEach("div[data-item-id]", func(_ int, el *colly.HTMLElement) {
-			fmt.Println("found an item inside")
-			strikedPrice := el.ChildText(".strike")
-			if strikedPrice == "" {
-				return
-			}
-			itemID := el.Attr("data-item-id")
-			name := el.ChildText(".title")
-			currentPrice := el.ChildText(".curr")
-			chatText <- strings.Join([]string{name, strikedPrice}, "\n")
-			fmt.Printf("Content found: %q \n %s \n %s \n %s \n", itemID, name, currentPrice, strikedPrice)
-		})
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	fmt.Println("Ready to visit localhost:8080...")
-	// ticker := time.NewTicker(5000 * time.Millisecond)
-	// for t := range ticker.C {
-	// fmt.Printf("[%02d:%02d:%02d]Tick! Visiting localhost:8080\n", t.Hour(), t.Minute(), t.Second())
-	c.Visit("http://localhost:8080")
-	// }
+	scraper.Start(msgChannel)
 
 	// /* Start the chatbot */
 	fmt.Println("Starting chatbot...")
@@ -72,7 +42,7 @@ func main() {
 		}
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		for item := range chatText {
+		for item := range msgChannel {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, item)
 			bot.Send(msg)
 		}
